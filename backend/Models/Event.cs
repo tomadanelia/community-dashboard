9c081873dc482d42192812ac6c83049ed47904f8
tomadanelia <tomadanelia16@gmail.com>
2025-11-07 16:23:24 +0400

    Add project files.

---
+++ backend/Models/Event.cs
@@ -0,0 +1,7 @@
+class Event
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+	public string Description { get; set; } = string.Empty;
+	public DateTime Date { get; set; }
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <p>Hello from frontend</p>
+    </div>
+  )
+}
+
+export default App
