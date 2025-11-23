eb733b0a12c1d1dbfca0c055f6c614090cff4342
tomadanelia <tomadanelia16@gmail.com>
2025-11-23 12:08:10 +0400

    better validation fields for event class

--- backend/Models/Event.cs
+++ backend/Models/Event.cs
@@ -8,10 +8,14 @@ namespace Backend.Models
     {
         public int Id { get; set; }
         [Required]
+        [StringLength(50, MinimumLength = 2,ErrorMessage ="name must be 2-50 characters")]
+
         public string Name { get; set; } = string.Empty;
+        [StringLength(500, ErrorMessage = "maximum 500 characters")]
 
         public string Description { get; set; } = string.Empty;
-        [Required]
+        [Required(ErrorMessage="date should be correct type")]
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
     }
 }
